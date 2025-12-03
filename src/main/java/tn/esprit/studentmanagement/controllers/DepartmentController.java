95dfc73625385819021438a0f9dae5c76ed523ea
hsouna <hsouna.sellami1@gmail.com>
2025-12-03 17:57:17 +0100

    Test commit pour pipeline Jenkins58

--- src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
+++ src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
@@ -8,7 +8,7 @@ import tn.esprit.studentmanagement.services.DepartmentService;
 import tn.esprit.studentmanagement.services.IDepartmentService;
 
 import java.util.List;
-/*hello mf 22 22*/
+/*hello mf 22 2222*/
 @RestController
 @RequestMapping("/Depatment")
 @CrossOrigin(origins = "http://localhost:4200")
